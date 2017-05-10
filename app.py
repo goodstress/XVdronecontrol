b6b2dc8ee42ddadb9b079d7b764755e11c27378a
viewratio <mgoverman19@riverdale.k12.or.us>
2017-05-10 07:56:05 -0700

    added another panel and changed oritenation to horizontal

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 class TestApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
+        layout = BoxLayout(orientation='horizontal')
         btn1 = Button(text='arm drone')
         btn2 = Button(text='takeoff')
         btn3 = Button(text='land')
+        btn4 = Button(text='disable motors')
         layout.add_widget(btn1)
-        layout.add_widget(btn2)
+        layout.add_widget(btn4)
         layout.add_widget(btn3)
+        layout.add_widget(btn2)
         return layout
 
 TestApp().run()
