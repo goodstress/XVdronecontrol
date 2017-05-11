58ee3767b0a3c1f68fe0ca05c3b7e15d11a57b29
viewratio <mgoverman19@riverdale.k12.or.us>
2017-05-10 22:00:48 -0700

    added auto updating values to a text label for motor speed in PWM signals

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
-from kivy.core.text import Label as CoreLabel
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
 class TestApp(App):
     def build(self):
         layout = BoxLayout(orientation='horizontal')
@@ -11,13 +13,17 @@ class TestApp(App):
         btn3 = Button(text='land')
         btn4 = Button(text='disable motors')
         s = Slider(min=1400, max=2000, value=1400)
-
         layout.add_widget(btn1)
         layout.add_widget(btn4)
         layout.add_widget(btn3)
         layout.add_widget(btn2)
         layout.add_widget(s)
-
+        inc = NumericProperty()
+        l = Label(text=str('1400'))
+        def OnSliderValueChange(instance,value):
+            l.text = str(value)
+        s.bind(value=OnSliderValueChange)
+        layout.add_widget(l)
         return layout
 
 TestApp().run()
