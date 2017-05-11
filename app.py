3fa7b92abebea3a8a96d6a35172f8e4059167d53
viewratio <mgoverman19@riverdale.k12.or.us>
2017-05-10 22:13:10 -0700

    added all 4 motors to the GUI and text that shows idle on application startup

--- app.py
+++ app.py
@@ -8,22 +8,37 @@ from kivy.properties import NumericProperty
 class TestApp(App):
     def build(self):
         layout = BoxLayout(orientation='horizontal')
-        btn1 = Button(text='arm drone')
-        btn2 = Button(text='takeoff')
-        btn3 = Button(text='land')
-        btn4 = Button(text='disable motors')
-        s = Slider(min=1400, max=2000, value=1400)
-        layout.add_widget(btn1)
-        layout.add_widget(btn4)
-        layout.add_widget(btn3)
-        layout.add_widget(btn2)
-        layout.add_widget(s)
+        s1 = Slider(min=1400, max=2000, value=1400, orientation='vertical')
+        s2 = Slider(min=1400, max=2000, value=1400, orientation='vertical')
+        s3 = Slider(min=1400, max=2000, value=1400, orientation='vertical')
+        s4 = Slider(min=1400, max=2000, value=1400, orientation='vertical')
+
         inc = NumericProperty()
-        l = Label(text=str('1400'))
-        def OnSliderValueChange(instance,value):
-            l.text = str(value)
-        s.bind(value=OnSliderValueChange)
-        layout.add_widget(l)
+
+        l1 = Label(text=str('idle'))
+        l2 = Label(text=str('idle'))
+        l3 = Label(text=str('idle'))
+        l4 = Label(text=str('idle'))
+        def ch_s1(instance,value):
+            l1.text = str('Motor 1: %s' % (value))
+        def ch_s2(instance,value):
+            l2.text = str('Motor 2: %s' % (value))
+        def ch_s3(instance,value):
+            l3.text = str('Motor 3: %s' % (value))
+        def ch_s4(instance,value):
+            l4.text = str('Motor 4: %s' % (value))
+        s1.bind(value=ch_s1)
+        s2.bind(value=ch_s2)
+        s3.bind(value=ch_s3)
+        s4.bind(value=ch_s4)
+        layout.add_widget(s1)
+        layout.add_widget(l1)
+        layout.add_widget(s2)
+        layout.add_widget(l2)
+        layout.add_widget(s3)
+        layout.add_widget(l3)
+        layout.add_widget(s4)
+        layout.add_widget(l4)
         return layout
 
 TestApp().run()
